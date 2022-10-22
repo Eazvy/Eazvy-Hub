0fe4326cc2b3d289a59e49ad50614a8210bce5ea
Eazvy <57739965+Eazvy@users.noreply.github.com>
2022-10-22 19:23:24 -0400

    Create Inviter.lua

---
+++ Inviter.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))().Prompt({name = "Eazvy-Hub",invite = "https://discord.gg/MC9Uxd5N"})
