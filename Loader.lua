c4facc5655724910fecf20e0658d5364ce4ced35
Eazvy <57739965+Eazvy@users.noreply.github.com>
2022-10-23 11:33:36 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -46,11 +46,17 @@ local Main = Window:MakeTab({
 Main:AddParagraph("Important","Eazvy-Hub is not responsible for in-game bans. If you are banned permanently, to not beg the owner/admins to help you or it'll result in a permanent blacklist.")
 Main:AddParagraph("Supported Executors:","Synapse X, KRNL, Scriptware, Fluxus. Some things may not function properly this all based off of your Executor! If your executor is not supported it may not be functional!")
 
+
+Main:AddTextbox({Name = "Enter Key",Default = "",TextDisappear = true,Callback = function(s)
+    _G.Key = tostring(s)
+end})
+
+
 Main:AddButton({Name = "Load Script",Callback = function()
-    local GetScript = Request({Url = "https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/Games/" .. game.PlaceId .. ".lua",Method = "GET"})
+    local GetScript = Request({Url = "https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/Games/" .. tostring(game.PlaceId) .. ".lua",Method = "GET"})
     if GetScript.Body ~= "404: Not Found" then
         Library:Destroy()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/Games/" .. game.PlaceId .. ".lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/Games/" .. tostring(game.PlaceId) .. ".lua"))()
     else
         SendNotification("Eazvy Hub - Error", "The game you're currently in is not supported with Eazvy Hub.", "rbxassetid://161551681", 5)
         return
