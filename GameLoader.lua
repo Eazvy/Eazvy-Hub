cbae21e0f912f8c4dfa3081bc8a286f82114758e
Eazvy <57739965+Eazvy@users.noreply.github.com>
2023-02-16 13:02:10 -0500

    Update GameLoader.lua

--- GameLoader.lua
+++ GameLoader.lua
@@ -2,13 +2,16 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
---library 
+--Library + Vars 
 local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Eazvy/Eazvy-Hub/main/Content/UILibrary.lua')))()
 local Request = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
+local Identifier = Request({Url = ('%sidentifier.brownfieldd00.workers.dev'):format('https://'), Method = 'GET'}).Body
+local Hash = '_B9219393lkdakdw12zx8c824021421-qwe9dwadwq3_'
+local RKey = tostring(os.date("*t").year)
+local User = game:GetService("Players").LocalPlayer.UserId
 
 
 --Functions
-
 local function SendNotification(Name,Content,Image,Time)
     Library:MakeNotification({
     Name = Name,
@@ -22,8 +25,6 @@ end
 
 
 SendNotification("Eazvy Hub", "Loading please wait...", "rbxassetid://10715004387", 3)
-
-
 
 local Window = Library:MakeWindow({
     Name = "Eazvy-Hub - Loader",
@@ -41,9 +42,17 @@ local Main = Window:MakeTab({
 	PremiumOnly = false
 })
 
+local Settings = Window:MakeTab({
+	Name = "Settings",
+	Icon = "rbxassetid://8382597378",
+	PremiumOnly = false
+})
+
+
+
 --Paragraphs 
-Main:AddParagraph("Important","Eazvy-Hub is not responsible for in-game bans. If you are banned permanently, to not beg the owner/admins to help you or it'll result in a permanent blacklist.")
-Main:AddParagraph("Supported Executors:","Synapse X, KRNL, Scriptware, Fluxus. Some things may not function properly this all based off of your Executor! If your executor is not supported it may not be functional!")
+Main:AddParagraph("Important","Eazvy-Hub is not responsible for in-game bans. If you are banned permanently, do not beg the owner to help you or it'll result in a permanent blacklist.")
+Main:AddParagraph("Supported Executors:","Synapse X, KRNL, Scriptware, Fluxus. Some things may not function properly this all based off of your Executor! If your executor is not supported some features may not be functional.")
 
 
 Main:AddTextbox({Name = "Enter Key",Default = "",TextDisappear = true,Callback = function(s)
@@ -60,4 +69,15 @@ Main:AddButton({Name = "Load Script",Callback = function()
         SendNotification("Eazvy Hub - Error", "The game you're currently in is not supported with Eazvy Hub.", "rbxassetid://161551681", 5)
         return
     end
+end})
+
+
+Settings:AddButton({Name = "Generate Key",Callback = function()
+    if setclipboard and Identifier then
+        SendNotification("Eazvy Hub - Success", "Set your unique-key to your clipboard. To view this key press Ctrl+V on your keyboard in a TextBox", "rbxassetid://4914902889", 5)
+        setclipboard(Hash..Identifier..User..RKey )
+    else 
+        SendNotification("Eazvy Hub - Error", "Failed to get key your executor is not supported.", "rbxassetid://161551681", 5)
+        return
+    end 
 end})
